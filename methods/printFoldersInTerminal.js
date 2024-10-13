9dfdce0e8eeed56cd488b5f91e19afb91240225a
Anhelina Mahidava <mahidavaanhelina@gmail.com>
2024-10-13 18:50:42 +0300

    Added logic for ls

--- methods/printFoldersInTerminal.js
+++ methods/printFoldersInTerminal.js
@@ -1,1 +1,19 @@
-export const printFoldersInTerminal = () => {};
+import fs from 'node:fs';
+import process from "node:process";
+
+export const printFoldersInTerminal = () => {
+	let structData = [];
+	fs.readdirSync(process.cwd(), { withFileTypes: true }).forEach(file => {
+
+		structData.push({ Name: file.name, Type: file.isDirectory() ? 'directory' : 'file' });
+	});
+
+	structData.sort((a, b) => {
+		if (a.Type === b.Type) {
+			return a.Name.localeCompare(b.Name);
+		}
+		return a.Type === 'directory' ? -1 : 1;
+	});
+
+	console.table(structData);
+};
