d6bf0608049ea7e98a11d9f93bec3254655ded46
Anhelina Mahidava <mahidavaanhelina@gmail.com>
2024-10-13 19:00:30 +0300

    Added logic for cat

--- methods/readFiles.js
+++ methods/readFiles.js
@@ -1,1 +1,17 @@
-export const readFiles = () => {};
+import fs from 'node:fs';
+import process from "node:process";
+import { join } from "path";
+
+export const readFiles = (userArguments) => {
+	try {
+		const rs = fs.createReadStream(join(process.cwd(), userArguments[0]));
+
+		rs.pipe(process.stdout);
+
+		rs.on('error', (err) => {
+			console.error('Error reading file:', err.message);
+		});
+	} catch (err) {
+		console.error(err.message);
+	}
+};
--- utils/getCommandAndParams.js
+++ utils/getCommandAndParams.js
@@ -1,7 +1,5 @@
 export const getCommandAndParams = (input) => {
-	const command = input.slice(0, 2);
-
-	const userArguments = input.slice(3).split(' ');
+	const [command, ...userArguments] = input.split(' ');
 
 	return [command, userArguments];
 };
