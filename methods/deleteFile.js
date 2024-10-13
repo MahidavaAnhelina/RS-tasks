d49b0e1f88884f9396a76df30e50369d0cd5c337
Anhelina Mahidava <mahidavaanhelina@gmail.com>
2024-10-13 22:49:45 +0300

    Added logic for delete file

--- methods/deleteFile.js
+++ methods/deleteFile.js
@@ -1,1 +1,22 @@
-export const deleteFile = () => {};
+import { cwd } from "node:process";
+import { createReadStream, existsSync, createWriteStream, unlink } from 'node:fs';
+import {basename, join} from "path";
+
+export const deleteFile = (userArguments) => {
+	try {
+		const path_to_file = join(cwd(), userArguments[0]);
+		if (!existsSync(path_to_file)) {
+			throw Error('File not exists');
+		}
+
+		unlink(path_to_file, (err) => {
+			if (err) {
+				console.error('Error removing file:', err.message);
+			} else {
+				console.log('File successfully deleted');
+			}
+		})
+	} catch (err) {
+		console.error(err);
+	}
+};
