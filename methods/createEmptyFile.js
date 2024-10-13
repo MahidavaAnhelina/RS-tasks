779102a59ca0229015214528ddf448c8775316ee
Anhelina Mahidava <mahidavaanhelina@gmail.com>
2024-10-13 19:03:09 +0300

    Added logic for add

--- methods/createEmptyFile.js
+++ methods/createEmptyFile.js
@@ -1,1 +1,11 @@
-export const createEmptyFile = () => {};
+import {join} from "path";
+import process from "node:process";
+import fs from 'node:fs';
+
+export const createEmptyFile = (userArguments) => {
+	try {
+		fs.closeSync(fs.openSync(join(process.cwd(), userArguments[0]), 'w'));
+	} catch (err) {
+		console.error(err.message);
+	}
+};
